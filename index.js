70cf81f0aafe4bde78f52b659d134f44b1e4f6d8
aron k koech <varokoaruna81@gmail.com>
2024-10-30 12:44:59 +0300

    adds static methods

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(val) {
+      if (typeof val !== 'string') return val; 
+      return val.charAt(0).toUpperCase() + val.slice(1);
+  }
+   static sanitize(val) {
+        if (typeof val !== 'string') return val; 
+        return val.replace(/[^a-zA-Z0-9' -]/g, '');
+    }
+    static titleize(val) {
+          if (typeof val !== 'string') return val; 
+  
+          const smallWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+          
+          return val
+              .toLowerCase() 
+              .split(' ') 
+              .map((word, index) => {
+                  
+                  if (index === 0 || !smallWords.includes(word)) {
+                      return word.charAt(0).toUpperCase() + word.slice(1);
+                  }
+                  return word; 
+              })
+              .join(' '); 
+      }
+  }
+  
+  
+  
+
